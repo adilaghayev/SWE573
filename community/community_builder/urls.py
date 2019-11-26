2e39cb7cf88b9a2c68ffeb119e946a454cc4a5bc
Adil_Aghayev <adilaghayev@gmail.com>
2019-11-26 17:31:51 +0300

    Posting functionality was created
    Posting functionality along with many ease of life features were implemented.

--- community/community_builder/urls.py
+++ community/community_builder/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from user import views as user_views
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='community-home'),
+    path('', PostListView.as_view(), name='community-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='community-about'),
-    path('register/', user_views.register, name='register'),
-
 ]
--- community/community_builder/views.py
+++ community/community_builder/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -8,6 +10,48 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'community_builder/home.html')
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'community_builder/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
